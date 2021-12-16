afbd4715d1f7c9110033d0ada020dc7db14c0bcc
Jaspier <dungoallen349@gmail.com>
2021-12-16 23:40:14 +0000

    Handling successful login with jwt and adding middleware to check for valid token

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/justinas/alice"
 )
+
+func (app *application) wrap(next http.Handler) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		ctx := context.WithValue(r.Context(), "params", ps)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	}
+}
 
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
+
+	secure := alice.New(app.checkToken)
 
 	router.HandlerFunc(http.MethodGet, "/status", app.statusHandler)
 
@@ -19,7 +30,9 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodGet, "/v1/genres", app.getAllGenres)
 
-	router.HandlerFunc(http.MethodPost, "/v1/admin/editsong", app.editSong)
+	router.POST("/v1/admin/editsong", app.wrap(secure.ThenFunc(app.editSong)))
+
+	// router.HandlerFunc(http.MethodPost, "/v1/admin/editsong", app.editSong)
 	router.HandlerFunc(http.MethodGet, "/v1/admin/deletesong/:id", app.deleteSong)
 
 	return app.enableCORS(router)
